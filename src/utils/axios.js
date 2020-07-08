d110567cd10577ea0698a40066560d7051c707b9
Olena Domchevska <olena.domchevska@altran.com>
2020-07-08 14:49:18 +0300

    fix herders

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,16 +3,14 @@ import axios from 'axios';
 const AxiosInstance = axios.create(
   {
     baseURL: `${process.env.VUE_APP_SERVER_HOST}/api/`,
-  }
+    headers: {
+      common: {
+          // 'Accept': '*/*',
+      },
+    },
+    withCredentials: true,
+  },
 );
-
-AxiosInstance.defaults.withCredentials = true;
-
-// Request interceptor
-AxiosInstance.interceptors.request.use(request => {
-  request.headers.common["Accept"] = '*/*';
-  return request
-});
 
 AxiosInstance.interceptors.response.use(null, async error => {
   return Promise.reject(error);
