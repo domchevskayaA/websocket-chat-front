224dce61f629ff52728996782cfaeafc5b117b80
Olena Domchevska <olena.domchevska@altran.com>
2020-07-09 16:45:30 +0300

    hardcode socket host

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   lintOnSave: false,
   devServer: {
+    host: process.env.VUE_APP_HOST,
+    port: process.env.VUE_APP_PORT,
+    https: true,
+    hot: true,
     disableHostCheck: true
   }
 }
