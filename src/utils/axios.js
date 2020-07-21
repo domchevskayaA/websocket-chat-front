990ecc572647ca67056d60bd0388400f85294382
Olena Domchevska <olena.domchevska@altran.com>
2020-07-21 13:47:16 +0300

    add authorization request header

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,17 +1,26 @@
 import axios from 'axios';
+import { getCookie } from './cookie';
 
 const AxiosInstance = axios.create(
   {
     baseURL: `${process.env.VUE_APP_SERVER_HOST}/api/`,
     headers: {
-      common: {
-          // 'Accept': '*/*',
-          // 'Access-Control-Request-Headers': 'content-type, origin',
-      },
+      common: {},
     },
     withCredentials: true,
   },
 );
+
+AxiosInstance.interceptors.request.use((config) => {
+  const token = getCookie('token');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
 
 AxiosInstance.interceptors.response.use(null, async error => {
   return Promise.reject(error);
