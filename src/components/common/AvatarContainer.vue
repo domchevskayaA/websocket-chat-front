6a7c1992c17ab4fe76b330399d70fe338c9354c3
Olena Domchevska <olena.domchevska@altran.com>
2020-07-23 16:22:51 +0300

    fix avatar size

---
+++ src/components/common/AvatarContainer.vue
@@ -0,0 +1,53 @@
+<template>
+    <div
+        class="image-container"
+        :style="{
+            height: `${size}px`,
+            width: `${size}px`,
+        }"    
+    >
+      <b-img
+        class="image"
+        :src="imageUrl || defaultImageUrl"
+        rounded="circle"
+      />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AvatarContainer',
+    props: {
+        size: {
+            type: Number,
+            default: 40,
+        },
+        imageUrl: {
+            required: true,
+            type: String,
+            default: '',
+        },
+        defaultImageUrl: {
+            type: String,
+            default: 'images/user-no-avatar.png',
+        }
+    },
+    data: () =>  {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+    .image-container {
+        overflow: hidden;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        .image {
+            height: 100%;
+        }
+    }
+</style>
