a8728af1efea881c18c20257812742860868148b
Olena Domchevska <olena.domchevska@altran.com>
2020-07-29 18:28:06 +0300

    fix unread messages, part 1

---
+++ src/store/modules/chats.js
@@ -0,0 +1,31 @@
+import axios from '@/utils/axios';
+import * as types from '../mutation-types';
+
+// state
+export const state = {
+  chats: null,
+};
+
+// getters
+export const getters = {
+  chats: state => state.chats,
+};
+
+// mutations
+export const mutations = {
+
+  [types.GET_CHATS](state, chats) {
+    state.chats = chats;
+  },
+};
+
+// actions
+export const actions = {
+
+  async getChats ({commit, state}, payload) {
+    const {data} = await axios.get('users');
+
+    await commit(types.GET_CHATS, data);
+    return data;
+  }
+};
